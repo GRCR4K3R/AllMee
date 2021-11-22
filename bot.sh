d301cd0511b7c11b53404dd800f76c5edfebee92
GRCR4K3R <94230332+GRCR4K3R@users.noreply.github.com>
2021-11-23 00:43:45 +0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -11,7 +11,14 @@ red='\033[31;1m'
 white='\033[37;1m'
 yellow='\033[33;1m'
 toilet -f big -F gay Trojan Malware
-figlet Reza alfauzan-XD
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+toilet -f big -F gay By Reza Alfauzan-Xd
 echo
 sleep 1
 echo -e $green"++++++++++++++++++++++++++++++++++++++++++++"
