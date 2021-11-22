f00f8de7ca0d2c6e3b9b0259e7887e58cd8d9103
GRCR4K3R <94230332+GRCR4K3R@users.noreply.github.com>
2021-11-22 12:18:02 +0700

    Update install.py

--- install.py
+++ install.py
@@ -6,11 +6,11 @@ def bersih():
         os.system("clear")
 
 def menu():
-        print "\t Tools By Reza Alfauzan\n"
-        print("1. Wajib update ya!!!")
-        print("2. Info Script.")
+        print "\t \033[36;1mTools \033[35;1mBy \033[33;1mReza \033[32;1mAlfauzan\n"
+        print("\033[32;1m1. \033[36;1mWajib update ya!!!")
+        print("\033[32;1m2. \033[36;1mInfo Script.")
         # input
-        pilih = input("Masukkan pilihan : ")
+        pilih = input("\033[36;1mMasukkan \033[32;1mpilihan : ")
         if pilih ==1:
                 os.system("pkg update && pkg upgrade")
                 os.system("pkg install nano")
@@ -28,11 +28,11 @@ def menu():
         if pilih ==2:
                 os.system("figlet Rzaa Ajaa")
                 logo = """
-                      ===============================================
-                      = (+) Reza Yang Buat Scriptnya             (+)=
-                      = (+) Github Gua :GRCR4K3R                 (+)=
-                      = (+) Facebook   :Rzaa Ajaa                (+)=
-                      = (+) Profil     :Stiker Pentol Itu gua!!  (+)=
+                      \033[32;1m===============================================
+                      \033[32;1m= (+) \033[36;1mReza Yang Buat Scriptnya             \033[32;1m(+)=
+                      \033[32;1m= (+) \033[36;1mGithub Gua :GRCR4K3R                 \033[32;1m(+)=
+                      \033[32;1m= (+) \033[36;1mFacebook   :Rzaa Ajaa                \033[32;1m(+)=
+                      \033[32;1m= (+) \033[36;1mProfil     :Stiker Pentol Itu gua!!  \033[32;1m(+)=
                       ==============================================="""
                 print logo
 menu()
